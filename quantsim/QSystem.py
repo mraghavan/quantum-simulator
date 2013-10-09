b9e11870433d4d4174212f4bc0e281f46b98dccb
Manish Raghavan <manishraghavan@gmail.com>
2013-10-09 11:54:42 -0700

    Use kronecker tensor products to handle 2-qubit gates

--- quantsim/QSystem.py
+++ quantsim/QSystem.py
@@ -10,42 +10,55 @@ class QSystem:
     def __init__(self, num_qubits=2):
         if num_qubits == 0:
             raise ValueError("System must have >= 0 qubits")
-        self.qubits = [0] * num_qubits
-        for i in range(num_qubits):
-            self.qubits[i] = Qubit(2)
+        self.N = num_qubits
+        self._state = np.array([0] * (2 ** num_qubits), dtype=complex)
+        self._state[0] = 1
 
-    @property
-    def num(self):
-        return len(self.qubits)
+    def single_gate(self, T, i):
+        if i < 0 or i >= self.N:
+            raise ValueError("Invalid qubit: " + str(i))
+        transforms = [(I, 1)] * self.N
+        transforms[i] = (T, 1)
+        self.transform(transforms)
 
-    def transform(self, T, first, second=None):
-        if first < 0 or first >= self.num:
-            raise ValueError("Invalid qubit " + str(first))
-        if second and (second < 0 or second >= self.num):
-            raise ValueError("Invalid qubit " + str(second))
-        if not second:              # single qubit gate
-            self.qubits[first].transform(T)
+    def double_gate(self, T, i):
+        if i < 0 or i >= self.N - 1:
+            raise ValueError("Invalid qubit: " + str(i))
+        transforms = [(I, 1)] * (self.N - 1)
+        transforms[i] = (T, 2)
+        self.transform(transforms)
+
+    def transform(self, transforms):
+        """Transforms is formatted like [(H, 1), (CNOT, 2), (I, 1)]"""
+        sum = 0
+        for transform, size in transforms:
+            if size != 1 and size != 2:
+                raise ValueError("Only one- or two-qubit gates allowed")
+            sum += size
+            utils.check_transform(transform, 2 ** size)
+        if sum != self.N:
+            raise ValueError("Wrong number of qubits")
+        T = self.create_transform([t[0] for t in transforms])
+        self.state = T.dot(self.state)
 
     @property
     def state(self):
-        base = self.qubits[0].state
-        for q in self.qubits[1:]:
-            base = utils.tensor(base, q.state)
-        return base
+        return self._state[:]
+
+    def create_transform(self, transforms):
+        """Transforms is formatted like [H, CNOT, I]"""
+        S = transforms[0]
+        for T in transforms[1:]:
+            S = np.kron(S, T)
+        return S
 
     def __repr__(self):
-        s = ''
-        for q in self.qubits:
-            s += repr(q)
-            s += '\n'
-        return s[:-1]
+        return str(self)
 
     def __str__(self):
         st = ''
         state = 0
         for s in self.state:
-            st += str(s) + '|' + "{0:b}".format(state) + '> + '
+            st += str(s) + '|' + "{0:{fill}{width}b}".format(state, fill='0', width=self.N) + '> + '
             state += 1
         return st[:-3]
-
-qs = QSystem()
--- quantsim/transforms.py
+++ quantsim/transforms.py
@@ -3,9 +3,16 @@ import numpy as np
 import cmath as cm
 from math import sin, cos
 
+# SINGLE QUBIT GATES
 H = np.array([[1,1],[1,-1]]) / cm.sqrt(2)
 NOT = np.array([[0,1],[1,0]])
 Z = np.array([[1,0],[0,-1]])
-
+I = np.array([[1,0],[0,1]])
 def U(theta):
     return np.array([[cos(theta),-sin(theta)],[sin(theta),cos(theta)]])
+
+# TWO QUBIT GATES
+CNOT = np.array([[1,0,0,0],
+                 [0,1,0,0],
+                 [0,0,0,1],
+                 [0,0,1,0]])
